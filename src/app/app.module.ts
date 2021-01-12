27f3f28dca57bb4ca7792e12c3e8e6b15949586e
christopher <angel.rmarin@outlook.com>
2021-01-12 15:03:52 -0600

    transfer component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TransferComponent } from './transfer/transfer.component';
 import { TransactionsTableComponent } from './transactions-table/transactions-table.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,14 +13,10 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     TransferComponent,
     TransactionsTableComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.css']
+  styleUrls: ['./transfer.component.css'],
 })
 export class TransferComponent implements OnInit {
+  transferForm = new FormGroup({
+    fromAccount: new FormControl('', Validators.required),
+    toAccount: new FormControl('', Validators.required),
+    amount: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitTransfer() {
+    console.log(this.transferForm.value);
   }
-
 }
